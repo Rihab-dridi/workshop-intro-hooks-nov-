6dfa0a9e5f853831d33f9a883493a481187e61b9
RIHAB <rihab.dridi.igs@gmail.com>
2021-12-29 17:16:55 +0100

    show handler

--- src/App.js
+++ src/App.js
@@ -1,42 +1,24 @@
 import React from "react";
 import "./App.css";
 import Counter from "./components/counter";
-
 import { Button } from "react-bootstrap";
+import { useState } from "react";
 
-class App extends React.Component {
-  //step1: declare a dynamic data + initialvalue=true
-  //step2: state management : true==> false/ false==> true
-
-  //phenomene cligneton: 2 parts
-  //step1: declare a boolean dynamic variable
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      show: true,
-    };
-  }
-  showHandler = () => {
-    this.setState({
-      show: !this.state.show,
-    });
+function App() {
+  const [show, setShow] = useState(false);
+  // const [timer,settimer]=useState(0)
+  const showHandler = () => {
+    setShow(!show);
   };
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Workshop state </h1>
-        <Button variant="danger" onClick={this.showHandler}>
-          {this.state.show ? "HIde" : "show"}{" "}
-        </Button>
-        {this.state.show && (
-          <>
-            <Counter />
-          </>
-        )}
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>Workshop state </h1>
+      <Button variant="danger" onClick={showHandler}>
+        {show ? "Hide" : "Show"}
+      </Button>
+      {show && <Counter />}
+    </div>
+  );
 }
 export default App;
